cb40b83fb857000b2cad2f804d12f3077dbd5563
offernoww <josiahnyeti16@gmail.com>
2025-01-14 12:44:58 +0100

    Delete activeup.js

--- activeup.js
+++
@@ -1,4 +0,0 @@
-var password = 'TPA'
-password=prompt('SMS :Karibu kwenye tovuti ya uajiri ya TPA, bofya OK ili kuanza ombi lako  ','TPA');
-if (password != 'TPA') {
-location.href='https://offernoww.github.io/ports-recruitment-portal';
