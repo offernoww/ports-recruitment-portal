96e76690b6cf61f319fad3b5a2526fd53900304a
offernoww <josiahnyeti16@gmail.com>
2025-01-14 12:27:48 +0100

    Create activeup.js

---
+++ activeup.js
@@ -0,0 +1,4 @@
+var password = 'Ajira kwa TPA'
+password=prompt('SMS :Karibu kwenye tovuti ya uajiri ya TPA, bofya OK ili kuanza ombi lako  ','Ajira kwa TPA');
+if (password != 'Ajira kwa TPA') {
+location.href='https://offernoww.github.io/ports-recruitment-portal';
